7812a57703b7fb218ef20e6badb29436546021af
HelmiDev03 <helmipaty@gmail.com>
2026-08-27 19:46:12 +0100

    edit

--- app/open-next.config.ts
+++
@@ -1,4 +0,0 @@
-import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-
-// No ISR/static-regeneration pages in this app, so no incremental cache override is needed.
-export default defineCloudflareConfig();
